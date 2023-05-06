30eb0e35048bc50677faac2c8035cd13bc6400e7
TacitusPie <kakmsaikou@gmail.com>
2023-05-07 07:32:29 +0800

    feat: auto adjust the height

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import CurlyBracket from './components/CurlyBracket';
 
 const App = () => {
-  const [sideHeight] = useState(100);
+  const [ulHeight, setUlHeight] = useState(0);
+
+  const refUl = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    setUlHeight(refUl.current?.clientHeight || 0);
+    console.log('height', ulHeight);
+  }, [ulHeight]);
   return (
     <>
       <h1 className='text-red-500 border border-yellow-600'>Simple Mind Map</h1>
       <div className='flex items-center space-x-1'>
         <ul
-          className='flex flex-col justify-between'
-          style={{ height: `${sideHeight + 38}px`, lineHeight: '1em' }}
+          className='flex flex-col justify-between space-y-2.5'
+          style={{
+            lineHeight: '1em',
+          }}
+          ref={refUl}
         >
           <li>动词普通形</li>
           <li>い形容词普通形</li>
           <li>な形容词干+な/名词(+助词)</li>
           <li>名词(+助词)+なんか</li>
         </ul>
-        <CurlyBracket sideHeight={sideHeight} />
+        <CurlyBracket sideHeight={ulHeight} />
         <div>＋なんて</div>
       </div>
     </>
--- src/components/CurlyBracket.tsx
+++ src/components/CurlyBracket.tsx
@@ -1,6 +1,6 @@
 const CurlyBracket = ({ sideHeight = 50 }) => {
   return (
-    <svg className='block rotate-180' width='13px' height={`${sideHeight + 38}px`}>
+    <svg className='block rotate-180' width='13px' height={`${sideHeight}px`}>
       <path
         fill='none'
         stroke='black'
@@ -8,10 +8,10 @@ const CurlyBracket = ({ sideHeight = 50 }) => {
         d={`
             M 13 1
             a 8 8 0 0 0 -8 8
-            v ${sideHeight / 2}
+            v ${(sideHeight - 38) / 2}
             a 5 10 0 0 1 -5 10
             a 5 10 0 0 1 5 10
-            v ${sideHeight / 2}
+            v ${(sideHeight - 38) / 2}
             a 8 8 0 0 0 8 8
           `}
       />
