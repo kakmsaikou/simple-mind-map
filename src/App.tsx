20bd32a9d87f428ca5af0136c7dc711d2679a14f
TacitusPie <kakmsaikou@gmail.com>
2023-05-10 21:39:44 +0800

    feat: edit root

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import CurlyBracket from './components/CurlyBracket';
 const App = () => {
   const [childNodesHeight, setUlHeight] = useState(0);
 
+  const [rootNode, setRootNode] = useState('＋なんて');
+
   const [childNodeList, setChildNodeList] = useState([
     '动词普通形',
     'い形容词普通形',
@@ -16,6 +18,11 @@ const App = () => {
   useEffect(() => {
     setUlHeight(childNodes.current?.clientHeight || 0);
   }, [childNodeList]);
+
+  const handleRootNodeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === '') return;
+    setRootNode(e.target.value);
+  };
 
   const handleChildNodeChange = (
     e: ChangeEvent<HTMLInputElement>,
@@ -36,7 +43,12 @@ const App = () => {
       <h1 className='text-red-500 border border-yellow-600'>Simple Mind Map</h1>
       <main className='flex'>
         <div>
-          <input type='text' placeholder='Input Root' />
+          <input
+            type='text'
+            placeholder='Input Root'
+            value={rootNode}
+            onInput={handleRootNodeChange}
+          />
           <ul>
             {childNodeList.map((_item, index) => (
               <li key={index}>
@@ -55,7 +67,7 @@ const App = () => {
             ))}
             <li>
               <button
-                className='btn btn-primary h-4'
+                className='btn btn-primary btn-sm w-1/2'
                 onClick={() => {
                   setChildNodeList([...childNodeList, '']);
                 }}
@@ -66,7 +78,7 @@ const App = () => {
           </ul>
         </div>
 
-        <div className='flex items-center space-x-1 bg-white text-black'>
+        <div className='flex items-center space-x-1 bg-white text-black px-2'>
           <ul
             className='flex flex-col justify-between space-y-2.5'
             style={{
@@ -79,7 +91,7 @@ const App = () => {
             ))}
           </ul>
           <CurlyBracket sideHeight={childNodesHeight} />
-          <div>＋なんて</div>
+          <div>{rootNode}</div>
         </div>
       </main>
     </>
